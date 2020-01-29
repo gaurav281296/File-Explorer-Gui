234d0f43600d148293413d745f61ed35fa742d05
Gaurav Agarwal <gaurav.agarwal@capitalfloat.com>
2020-01-29 14:10:57 +0530

    Handled output for invalid or non existant paths.

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -2,13 +2,19 @@ function operate(operator) {
 	var path = document.querySelector('#path').value;
 	resultLambda = operator(path);
 	resultLambda(result => {
-		ul = document.createElement('ul');
-		document.getElementById('output').appendChild(ul);
-		result.forEach(function (res) {
-			let li = document.createElement('li');
-			ul.appendChild(li);
-			li.innerHTML += res;
-		});
-		//document.querySelector('#output').innerText = result;
+		document.getElementById("output").innerHTML = "";
+		
+		if (typeof result !== 'undefined' && result.length > 0) {
+			ul = document.createElement('ul');
+			document.getElementById('output').appendChild(ul);
+			result.forEach(function(res) {
+				let li = document.createElement('li');
+				ul.appendChild(li);
+				li.innerHTML += res;
+			});
+		}
+		else {
+			document.querySelector('#output').innerText = "Directory with the given path does not exist or is invalid";
+		}
 	});
 }
